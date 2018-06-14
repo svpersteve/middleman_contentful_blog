f3a32a67046cbcc828c326b294a77a1b6c9e92af
Steve Brewer <steve.v.brewer@gmail.com>
2018-06-14 17:43:25 +0300

    Install Middleman

---
+++ config.rb
@@ -0,0 +1,12 @@
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+configure :build do
+end
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
